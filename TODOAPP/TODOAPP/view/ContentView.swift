5c58a3ab8c518d2006ea0022bda37ebeb808c01b
최형우 <a11dsd123@gmail.com>
2020-01-06 17:26:47 +0700

    update

--- TODOAPP/TODOAPP/view/ContentView.swift
+++ TODOAPP/TODOAPP/view/ContentView.swift
@@ -4,43 +4,53 @@
 //
 //  Created by Choi on 2019/12/13.
 //  Copyright © 2019 motivatin. All rights reserved.
-//
 
+import Firebase
 import SwiftUI
 
 struct TodoListView: View {
-    @State var todos = [
-        Todo(title: "Study SwiftUI, Combine", isDone: true),
-        Todo(title: "Solve Jsceno Issue #33", isDone: false),
-        Todo(title: "Write TIL before off work", isDone: false),
-        Todo(title: "Buy Kotlin with Functional programming", isDone: true),
-        Todo(title: "Remove Visual Studio Code", isDone: false),
-    ]
-    
-    let defaultTodo = Todo(title: "Enter the title", isDone: false)
-    
-    var body: some View {
-        NavigationView {
-            List(todos.indices, id:\.self){ index in
-                NavigationLink(destination: DetailView(todo: self.$todos[index])){
-                    TodoRow(todo: self.$todos[index])
-                }
-            }
-            .navigationBarTitle("오늘 할 일")
-            .navigationBarItems(trailing: Button(action: {self.didTapAddButton()}) {
-                Image(systemName: "plus")
-                    .foregroundColor(.white)
-            })
+  let db = Firestore.firestore()
+  let defaultTodo = Todo(title: "Enter the title", isDone: false)
+
+  var ref: DocumentReference?
+  @State var todos: [Todo] = []
+
+  init() {
+    ref = db.collection("todo").addDocument(data: [
+        "title": "Jsceno Issue #75",
+        "isDone": false,
+        "date": Date()
+    ]) { err in
+        if let err = err {
+            print("Error adding document: \(err)")
+        } else {
+            print("Document added")
         }
     }
-    
-    func didTapAddButton(){
-        self.todos.append(defaultTodo)
+  }
+
+  var body: some View {
+    NavigationView {
+      List(todos.indices, id: \.self) { index in
+        NavigationLink(destination: DetailView(todo: self.$todos[index])) {
+          TodoRow(todo: self.$todos[index])
+        }
+      }
+      .navigationBarTitle("오늘 할 일")
+      .navigationBarItems(trailing: Button(action: { self.didTapAddButton() }) {
+        Image(systemName: "plus")
+          .foregroundColor(.white)
+      })
     }
+  }
+
+  func didTapAddButton() {
+    todos.append(defaultTodo)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        TodoListView()
-    }
+  static var previews: some View {
+    TodoListView()
+  }
 }
