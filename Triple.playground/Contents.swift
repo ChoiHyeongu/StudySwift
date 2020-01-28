c18eee1d8fa4bcf6d74da0e208153611a4d2c7a1
최형우 <a11dsd123@gmail.com>
2020-01-28 11:42:07 +0900

    update

---
+++ Triple.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class Triple<A: Any, B: Any, C: Any> {
+  var first: A
+  var second: B
+  var third: C
+
+  init(_ first: A, _ second: B, _ third: C) {
+    self.first = first
+    self.second = second
+    self.third = third
+  }
+
+  func toList() -> [Any] {
+    return [first, second, third]
+  }
+
+  func toString() -> String {
+    return "(\(first), \(second), \(third))"
+  }
+}
+
+let triple = Triple(1, "A", "Choi")
+
+print(triple.toList())
+print(triple.toString())
