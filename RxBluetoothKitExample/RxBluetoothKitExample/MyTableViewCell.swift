1843e1ba99a9184ff94651fafa5f53041c4c10c8
최형우 <a11dsd123@gmail.com>
2020-03-12 17:18:49 +0900

    Update

---
+++ RxBluetoothKitExample/RxBluetoothKitExample/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  RxBluetoothKitExample
+//
+//  Created by Choi on 2020/02/28.
+//  Copyright © 2020 Choi. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
